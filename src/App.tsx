dc8e97ca1ba2786a559e290c4b376a48d5beb494
ShaikhHossain <Shaikh.Hossain@target.com>
2025-05-02 17:20:09 +0530

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { ArtistProvider } from './context/ArtistContext';
 import { BookingProvider } from './context/BookingContext';
 import { Navbar } from './components/Navbar';
@@ -12,20 +12,18 @@ function App() {
   return (
     <ArtistProvider>
       <BookingProvider>
-        <Router>
-          <div className="min-h-screen bg-black text-white">
-            <Navbar />
-            <main>
-              <Routes>
-                <Route path="/" element={<ArtistListPage />} />
-                <Route path="/artist/:id" element={<ArtistDetail />} />
-                <Route path="/compare" element={<ComparisonView />} />
-                <Route path="/cart" element={<BookingCart />} />
-                <Route path="/admin" element={<AdminDashboard />} />
-              </Routes>
-            </main>
-          </div>
-        </Router>
+        <div className="min-h-screen bg-black text-white">
+          <Navbar />
+          <main>
+            <Routes>
+              <Route path="/" element={<ArtistListPage />} />
+              <Route path="/artist/:id" element={<ArtistDetail />} />
+              <Route path="/compare" element={<ComparisonView />} />
+              <Route path="/cart" element={<BookingCart />} />
+              <Route path="/admin" element={<AdminDashboard />} />
+            </Routes>
+          </main>
+        </div>
       </BookingProvider>
     </ArtistProvider>
   );
